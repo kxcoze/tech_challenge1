bec3d4aca82b603356f97667ccdfc90b3c6d55f5
kxcoze <zakharloschev@gmail.com>
2023-10-12 13:12:48 +0300

    Revert "initial"
    This reverts commit 16f7028d12b2a9636282c8e34f00973868251981.

--- db/session.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-from src.config import config
-
-engine = create_engine(config.POSTGRES_URL)
-
-SessionLocal = sessionmaker(auto_commit=False, auto_flush=False, bind=engine)
